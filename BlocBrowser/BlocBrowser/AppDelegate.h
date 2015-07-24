9924faaaf245670e7f792e8fa3aad36f790ba5f2
Chad Clayton <chad@kcwaverider.com>
2015-07-23 22:12:32 -0500

    Finished Building Bloc Browser

---
+++ BlocBrowser/BlocBrowser/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BlocBrowser
+//
+//  Created by Chad Clayton on 7/21/15.
+//  Copyright (c) 2015 Chad Clayton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
