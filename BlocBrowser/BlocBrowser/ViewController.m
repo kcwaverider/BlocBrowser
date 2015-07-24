576939e37d87f6f5d78e6985e503b4416f976db4
Chad Clayton <chad@kcwaverider.com>
2015-07-24 00:24:37 -0500

    completed assignment 24, intro message

--- BlocBrowser/BlocBrowser/ViewController.m
+++ BlocBrowser/BlocBrowser/ViewController.m
@@ -31,6 +31,8 @@
     self.webView = [WKWebView new];
     self.webView.navigationDelegate = self;
     
+    [self welcomeMessageSplash];
+    
     self.textField = [UITextField new];
     self.textField.keyboardType = UIKeyboardTypeURL;
     self.textField.returnKeyType = UIReturnKeyDone;
@@ -53,17 +55,11 @@
     [self.reloadButton setEnabled:NO];
     
     [self.backButton setTitle:NSLocalizedString(@"Back", @"Back Command") forState:UIControlStateNormal];
-    [self.backButton addTarget:self.webView action:@selector(goBack) forControlEvents:UIControlEventTouchUpInside];
-    
     [self.forwardButton setTitle:NSLocalizedString(@"Forward", @"Forward Command") forState:UIControlStateNormal];
-    [self.forwardButton addTarget:self.webView action:@selector(goForward) forControlEvents:UIControlEventTouchUpInside];
-    
     [self.stopButton setTitle:NSLocalizedString(@"Stop", @"Stop Command") forState:UIControlStateNormal];
-    [self.stopButton addTarget:self.webView action:@selector(stopLoading) forControlEvents:UIControlEventTouchUpInside];
-   
-    
     [self.reloadButton setTitle:NSLocalizedString(@"Refresh", @"Reload command") forState:UIControlStateNormal];
-    [self.reloadButton addTarget:self.webView action:@selector(reload) forControlEvents:UIControlEventTouchUpInside];
+    [self addButtonTargets];
+
     
     
     for (UIView *viewToAdd in @[self.webView, self.textField, self.backButton, self.forwardButton, self.stopButton, self.reloadButton]) {
@@ -189,8 +185,46 @@
     self.backButton.enabled = [self.webView canGoBack];
     self.forwardButton.enabled = [self.webView canGoForward];
     self.stopButton.enabled = self.webView.isLoading;
-    self.reloadButton.enabled  = !self.webView.isLoading;
+    self.reloadButton.enabled  = !self.webView.isLoading && self.webView.URL;
 }
+
+- (void) resetWebView {
+    [self.webView removeFromSuperview];
+    
+    WKWebView *newWebView = [WKWebView new];
+    newWebView.navigationDelegate = self;
+    [self.view addSubview:newWebView];
+    
+    self.webView = newWebView;
+    
+    [self addButtonTargets];
+    
+    self.textField.text = nil;
+    [self updateButtonsAndTitle];
+}
+
+- (void) addButtonTargets {
+    for (UIButton *button in @[self.backButton, self.forwardButton, self.stopButton, self.reloadButton]) {
+        [button removeTarget:nil action:NULL forControlEvents:UIControlEventTouchUpInside];
+    }
+    
+    [self.backButton addTarget:self.webView action:@selector(goBack) forControlEvents:UIControlEventTouchUpInside];
+    [self.forwardButton addTarget:self.webView action:@selector(forwardButton) forControlEvents:UIControlEventTouchUpInside];
+    [self.stopButton addTarget:self.webView action:@selector(stopButton) forControlEvents:UIControlEventTouchUpInside];
+    [self.reloadButton addTarget:self.webView action:@selector(reload) forControlEvents:UIControlEventTouchUpInside];
+}
+
+- (void) welcomeMessageSplash {
+    UIAlertController *welcomeMessage = [UIAlertController alertControllerWithTitle:NSLocalizedString(@"Welcome to BlocBrowser!", @"Welcome message") message:NSLocalizedString(@"The most PRIVATE browser on your phone", @"App description") preferredStyle:UIAlertControllerStyleAlert];
+    
+    UIAlertAction *messageAction = [UIAlertAction actionWithTitle:NSLocalizedString(@"Got It", nil) style:UIAlertActionStyleCancel handler:nil];
+    
+    [welcomeMessage addAction:messageAction];
+    
+    [self presentViewController:welcomeMessage animated:YES completion:nil];
+}
+
+
 
 @end
 
